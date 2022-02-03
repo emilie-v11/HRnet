09dc0852ccfc7ae7b25d08422186b02e8dcbde7c
Vukovic Emilie <emilievukovic79@gmail.com>
2022-02-03 23:30:33 +0100

    Footer - Fix responsive

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <footer className="text-center">
+        <footer className="text-center m-3">
             <small className="fst-italic fw-lighter">
-                © {new Date().getFullYear()} Wealth Health Company, Inc. All rights reserved.
+                © {new Date().getFullYear()} Wealth Health Company, Inc. <br />
+                All rights reserved.
             </small>
         </footer>
     );
